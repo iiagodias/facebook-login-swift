aae066dd1ca028bb2cc1dc29dd5925157b950244
Iago Dias <iago.unama@hotmail.com>
2023-06-11 14:26:29 -0300

    Layout facebook login page

--- facebookLogin/ContentView.swift
+++ facebookLogin/ContentView.swift
@@ -8,14 +8,70 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var email: String = ""
+    @State var password: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            Color("BackgroundColor")
+                .ignoresSafeArea()
+            
+            VStack{
+
+                Image("logo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 280)
+                
+                Spacer().frame(height: 64)
+                
+                VStack{
+                    TextField("E-mail",text: $email)
+                        .padding(.top, 24)
+                        .padding(.bottom, 12)
+                        .padding(.horizontal)
+                        .keyboardType(.emailAddress)
+                    Divider()
+                    SecureField("Senha",text: $password)
+                        .padding(.top, 12)
+                        .padding(.bottom, 24)
+                        .padding(.horizontal)
+                }.background(.white)
+                    .cornerRadius(4.0)
+                
+                Spacer().frame(height: 16)
+                
+                Button(action: {}) {
+                    Text("Login")
+                        .font(.title3)
+                        .bold()
+                        .foregroundColor(.white)
+                        .padding(20)
+                        .frame(maxWidth: .infinity)
+                        .background(Color("ButtonColor"))
+                        .cornerRadius(4.0)
+                }
+                
+                Spacer().frame(height: 64)
+                
+                VStack(spacing: 8){
+                    Button(action: {}) {
+                        Text("Don' t have an account? Sign up.")
+                            .foregroundColor(.white)
+                            .bold()
+                    }
+                    
+                    Button(action: {}) {
+                        Text("Forgot your password.")
+                            .foregroundColor(Color("TextColor"))
+                    }
+                }
+                
+               
+            }.padding()
+            
         }
-        .padding()
     }
 }
 
