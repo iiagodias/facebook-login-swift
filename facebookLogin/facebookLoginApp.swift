7cc81a7a984cd8e877af38c9cc5fc080e1c498b7
Iago Dias <iago.unama@hotmail.com>
2023-06-11 13:31:07 -0300

    Initial Commit

---
+++ facebookLogin/facebookLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  facebookLoginApp.swift
+//  facebookLogin
+//
+//  Created by Iago Dias on 11/06/23.
+//
+
+import SwiftUI
+
+@main
+struct facebookLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
